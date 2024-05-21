8c33fd551d1622e9ff0739110980b7931b3b5296
Bingan Chen <cnbga@outlook.com>
2024-05-20 21:48:17 -0700

    functions for lstm

--- dataloader.py
+++ dataloader.py
@@ -80,3 +80,94 @@ def make_data_loader(data, labels, batch_size, shuffle=True):
 
     return DataLoader(dataset, batch_size=batch_size, shuffle=shuffle)
 
+def create_time_series_dataloaders(data, labels, batch_size, n_splits=5):
+    """
+    Creates DataLoader objects for time series data with time-based splitting.
+
+    Args:
+    - data (array-like): The time series data.
+    - labels (array-like): The corresponding labels.
+    - batch_size (int): The batch size for DataLoader objects.
+    - n_splits (int): The number of splits for time series splitting (default: 5).
+
+    Returns:
+    - dataloaders (list): A list of DataLoader objects for each split.
+    - train_indices_list (list): A list of train indices for each split.
+    - test_indices_list (list): A list of test indices for each split.
+    - train_data_list (list): A list of train data arrays for each split.
+    - test_data_list (list): A list of test data arrays for each split.
+    - train_labels_list (list): A list of train label arrays for each split.
+    - test_labels_list (list): A list of test label arrays for each split.
+    """
+    class CustomTimeSeriesDataset(Dataset):
+        def __init__(self, data, labels, train_indices, test_indices):
+            self.train_data = [data[i] for i in train_indices]
+            self.train_labels = [labels[i] for i in train_indices]
+            self.test_data = [data[i] for i in test_indices]
+            self.test_labels = [labels[i] for i in test_indices]
+
+        def __len__(self):
+            return len(self.train_data) + len(self.test_data)
+
+        def __getitem__(self, idx):
+            if idx < len(self.train_data):
+                return self.train_data[idx], self.train_labels[idx]
+            else:
+                test_idx = idx - len(self.train_data)
+                return self.test_data[test_idx], self.test_labels[test_idx]
+        
+    tscv = TimeSeriesSplit(n_splits=n_splits)
+    train_indices_list = []
+    test_indices_list = []
+    train_data_list = []
+    test_data_list = []
+    train_labels_list = []
+    test_labels_list = []
+
+    for train_index, test_index in tscv.split(data):
+        train_indices_list.append(train_index)
+        test_indices_list.append(test_index)
+        train_data_list.append([data[i] for i in train_index])
+        test_data_list.append([data[i] for i in test_index])
+        train_labels_list.append([labels[i] for i in train_index])
+        test_labels_list.append([labels[i] for i in test_index])
+
+    dataloaders = []
+    for train_indices, test_indices in zip(train_indices_list, test_indices_list):
+        dataset = CustomTimeSeriesDataset(data, labels, train_indices, test_indices)
+        dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+        dataloaders.append(dataloader)
+
+    return dataloaders, train_indices_list, test_indices_list, train_data_list, test_data_list, train_labels_list, test_labels_list
+
+def LSTM_model_maker(input_size, hidden_size, num_layers, output_size):
+    class LSTMModel(nn.Module):
+        def __init__(self, input_size, hidden_size, num_layers, output_size):
+            super(LSTMModel, self).__init__()
+            self.hidden_size = hidden_size
+            self.num_layers = num_layers
+            self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
+            self.fc = nn.Linear(hidden_size, output_size)
+
+        def forward(self, x):
+            h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(x.device)
+            c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(x.device)
+            out, _ = self.lstm(x, (h0, c0))
+            out = self.fc(out[:, -1, :])
+            return out
+
+    return LSTMModel(input_size, hidden_size, num_layers, output_size)
+
+def train_model(model, train_loader, num_epochs, criterion, optimizer):
+    model.train()
+    for epoch in range(num_epochs):
+        for i, (x, y) in enumerate(train_loader):
+            x = x.view(x.size(0), -1, x.size(-1)).float()  
+            y = y.view(-1, 1).float()  
+            outputs = model(x)
+            loss = criterion(outputs, y)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+        print(f'Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item():.4f}')
